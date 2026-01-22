0dca50c983a2f0e6dea563ec5e74bc42466dfd1b
warpyn <karthik.r.varadharajan@gmail.com>
2026-01-22 11:39:41 -0500

    zsh config with cleaner formatting and os arch checks

--- zsh/.zprofile
+++ zsh/.zprofile
@@ -1,1 +1,8 @@
-eval "$(/opt/homebrew/bin/brew shellenv)"
+arch=$(uname -m)
+if [[ "$OSTYPE" == darwin* ]]; then
+    if [[ "$arch" == "arm64" ]]; then
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    elif [[ "$arch" == "x86_64" ]]; then
+        eval "$(/usr/local/bin/brew shellenv)"
+    fi
+fi
--- zsh/.zshcustom/aws_creds.sh
+++ zsh/.zshcustom/aws_creds.sh
@@ -1,4 +1,6 @@
-KEY_ID=$(grep -m 1 'aws_access_key_id' ~/.aws/credentials | cut -d'=' -f2 | tr -d '[:space:]')
-SECRET_KEY=$(grep -m 1 'aws_secret_access_key' ~/.aws/credentials | cut -d'=' -f2 | tr -d '[:space:]')
-export AWS_ACCESS_KEY_ID=$KEY_ID
-export AWS_SECRET_ACCESS_KEY=$SECRET_KEY
+if command -v aws > /dev/null 2>&1; then
+    KEY_ID=$(grep -m 1 'aws_access_key_id' ~/.aws/credentials | cut -d'=' -f2 | tr -d '[:space:]')
+    SECRET_KEY=$(grep -m 1 'aws_secret_access_key' ~/.aws/credentials | cut -d'=' -f2 | tr -d '[:space:]')
+    export AWS_ACCESS_KEY_ID=$KEY_ID
+    export AWS_SECRET_ACCESS_KEY=$SECRET_KEY
+fi
